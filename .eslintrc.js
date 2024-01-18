a4ccac23c931855536756fab626530cb84ada58d
Timur Khrustalyov <hrustalevdev@gmail.com>
2024-01-18 13:19:56 +0300

    Add `eslint-plugin-import`

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
@@ -24,6 +26,55 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
-  rules: {},
+  plugins: ['@typescript-eslint', 'import'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+      node: true,
+    },
+  },
+  rules: {
+    'import/no-commonjs': 'off',
+    'import/no-deprecated': 'warn',
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          orderImportKind: 'desc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+        warnOnUnassignedImports: true,
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+        ],
+      },
+    ],
+    'import/first': 'error',
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/newline-after-import': 'error',
+    'import/no-useless-path-segments': 'error',
+  },
 };
