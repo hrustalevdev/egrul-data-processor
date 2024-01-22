10ef88ac744e40621e86f7a0b6694f269c2a7a44
Timur Khrustalyov <hrustalevdev@gmail.com>
2024-01-22 11:06:11 +0300

    Add `xmlProcess` helper

--- src/enrich/enrich.ts
+++ src/enrich/enrich.ts
@@ -1,12 +1,11 @@
-import iconv from 'iconv-lite';
 import JSZip from 'jszip';
 import fs from 'node:fs';
 import path from 'node:path';
 import ProgressBar from 'progress';
-import sax from 'sax';
 
-import { db, Organization, OrganizationBuilder } from '../db';
-import type { IOrganization } from '../db';
+import { db } from '../db';
+
+import { xmlProcess } from './lib/xmlProcess';
 
 export const enrich = async (inputFolderPath: string) => {
   await db.connect();
@@ -43,69 +42,14 @@ export const enrich = async (inputFolderPath: string) => {
       },
     );
 
+    const tick = progressBar.tick.bind(progressBar);
+
     for (let j = 0; j < xmlFiles.length; j++) {
       const xmlFile = xmlFiles[j];
-      const saxStream = sax.createStream(true, { trim: true });
-      const xmlStream = xmlFile
-        .nodeStream()
-        .pipe(iconv.decodeStream('win1251'))
-        .pipe(saxStream);
-
-      const organizations: IOrganization[] = [];
-      let organization: OrganizationBuilder | null = null;
-
-      xmlStream.on('opentag', (node) => {
-        switch (node.name) {
-          case 'СвЮЛ': {
-            if (organization) organizations.push(organization.build());
-
-            const ogrn = node.attributes['ОГРН'] as string;
-            const ogrnDate = node.attributes['ДатаОГРН'] as string;
-            const opf = node.attributes?.['ПолнНаимОПФ'] as string;
-            const inn = node.attributes?.['ИНН'] as string;
-            const kpp = node.attributes?.['КПП'] as string;
-
-            organization = new OrganizationBuilder(
-              ogrn,
-              ogrnDate,
-              opf,
-              inn,
-              kpp,
-            );
-            break;
-          }
-          case 'СвНаимЮЛ': {
-            const fullName = node.attributes['НаимЮЛПолн'] as string;
-            organization?.setFullName(fullName);
-            break;
-          }
-          case 'СвНаимЮЛСокр': {
-            const shortName = node.attributes['НаимСокр'] as string;
-            organization?.setShortName(shortName);
-            break;
-          }
-        }
-      });
-
-      xmlStream.on('error', (error) => {
-        console.error(error);
-      });
-
-      xmlStream.on('end', () => {
-        if (organization) organizations.push(organization.build());
-        Organization.insertMany(organizations);
-      });
-
-      await new Promise((resolve) => xmlStream.on('end', resolve));
-      progressBar.tick();
-
       const isLastFile =
         i === zipFilePaths.length - 1 && j === xmlFiles.length - 1;
 
-      if (isLastFile) {
-        console.log('Processing finished');
-        await db.disconnect();
-      }
+      await xmlProcess(xmlFile, isLastFile, tick);
     }
   }
 };
---
+++ src/enrich/lib/xmlProcess.ts
@@ -0,0 +1,66 @@
+import iconv from 'iconv-lite';
+import type JSZip from 'jszip';
+import sax from 'sax';
+
+import { db, Organization, OrganizationBuilder } from '../../db';
+import type { IOrganization } from '../../db';
+
+export const xmlProcess = async (
+  xmlFile: JSZip.JSZipObject,
+  isLastFile: boolean,
+  onProgress: () => void,
+) => {
+  const saxStream = sax.createStream(true, { trim: true });
+  const xmlStream = xmlFile
+    .nodeStream()
+    .pipe(iconv.decodeStream('win1251'))
+    .pipe(saxStream);
+
+  const organizations: IOrganization[] = [];
+  let organization: OrganizationBuilder | null = null;
+
+  xmlStream.on('opentag', (node) => {
+    switch (node.name) {
+      case 'СвЮЛ': {
+        if (organization) organizations.push(organization.build());
+
+        const ogrn = node.attributes['ОГРН'] as string;
+        const ogrnDate = node.attributes['ДатаОГРН'] as string;
+        const opf = node.attributes?.['ПолнНаимОПФ'] as string;
+        const inn = node.attributes?.['ИНН'] as string;
+        const kpp = node.attributes?.['КПП'] as string;
+
+        organization = new OrganizationBuilder(ogrn, ogrnDate, opf, inn, kpp);
+        break;
+      }
+      case 'СвНаимЮЛ': {
+        const fullName = node.attributes['НаимЮЛПолн'] as string;
+        organization?.setFullName(fullName);
+        break;
+      }
+      case 'СвНаимЮЛСокр': {
+        const shortName = node.attributes['НаимСокр'] as string;
+        organization?.setShortName(shortName);
+        break;
+      }
+    }
+  });
+
+  xmlStream.on('error', (error) => {
+    console.error(error);
+    throw error;
+  });
+
+  xmlStream.on('end', () => {
+    if (organization) organizations.push(organization.build());
+    Organization.insertMany(organizations);
+  });
+
+  await new Promise((resolve) => xmlStream.on('end', resolve));
+  onProgress();
+
+  if (isLastFile) {
+    console.log('Processing finished');
+    await db.disconnect();
+  }
+};
