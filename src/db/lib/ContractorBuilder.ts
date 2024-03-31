d6f8e040e60f65a6690185d1a708fef21d3fbd50
Timur Khrustalyov <hrustalevdev@gmail.com>
2024-03-31 11:39:08 +0300

    Add base address builder

--- src/db/lib/ContractorBuilder.ts
+++ src/db/lib/ContractorBuilder.ts
@@ -7,9 +7,32 @@ import type {
   UStatus,
   UType,
 } from '../types';
-import type { IAddressItem } from '../types/addresses';
+
+interface IAddress {
+  postalCode?: string;
+  regionId?: string;
+  region?: string;
+  /** Муниципальный район */
+  municipalArea?: string;
+  /** Город, село, поселение */
+  settlement?: string;
+  /** Населённый пункт */
+  locality?: string;
+  /** Элемент планировочной структуры (р-н, мкр-н) */
+  district?: string;
+  /** Элемент улично-дорожной сети (ул., ш. и т.п.)*/
+  street?: string;
+  /** Здание/сооружение */
+  building?: string;
+  /** Помещение в пределах здания, сооружения */
+  apartment?: string;
+  /** Помещение в пределах квартиры */
+  room?: string;
+}
 
 export class ContractorBuilder implements TFullOrganizationDataItem {
+  private readonly _address: IAddress = {};
+
   constructor(
     public value: string,
     public unrestricted_value: string,
@@ -25,6 +48,7 @@ export class ContractorBuilder implements TFullOrganizationDataItem {
    * @link - https://sudact.ru/law/prikaz-fns-rossii-ot-18012021-n-ed-7-1417/
    * C - tag
    * A - attr
+   * П - text
    * O - required
    * H - optional
    */
@@ -91,25 +115,78 @@ export class ContractorBuilder implements TFullOrganizationDataItem {
     return this;
   }
 
+  /** C: СведДолжнФЛ; C_H: СвФЛ; A_H: Фамилия, Имя, Отчество */
   setManagementName(name: string) {
     this.data.management = { name, post: '' };
     return this;
   }
 
+  /** C: СведДолжнФЛ; C_H: СвДолжн; A_O: НаимДолжн */
   setManagementPost(post: string) {
     this.data.management.post = post;
     return this;
   }
 
-  setAddress(address: string) {
-    this.data.address = {
-      value: address,
-      unrestricted_value: address,
-      data: {} as IAddressItem['data'],
-    };
-
+  /** C: СвАдрЮЛФИАС; A_H: Индекс */
+  setAddrPostalCode(code: string) {
+    this._address.postalCode = code;
     return this;
   }
+
+  /** С_П: Регион */
+  setAddrRegionId(id: string) {
+    this._address.regionId = id;
+    return this;
+  }
+
+  /** С_П: НаимРегион */
+  setAddrRegion(id: string) {
+    this._address.region = id;
+    return this;
+  }
+
+  setMunicipalArea(area: string) {
+    this._address.municipalArea = area;
+    return this;
+  }
+  setSettlement(settlement: string) {
+    this._address.settlement = settlement;
+    return this;
+  }
+  setLocality(locality: string) {
+    this._address.locality = locality;
+    return this;
+  }
+  setDistrict(district: string) {
+    this._address.district = district;
+    return this;
+  }
+  setStreet(street: string) {
+    this._address.street = street;
+    return this;
+  }
+  setBuilding(building: string) {
+    this._address.building = building;
+    return this;
+  }
+  setApartment(apartment: string) {
+    this._address.apartment = apartment;
+    return this;
+  }
+  setRoom(room: string) {
+    this._address.room = room;
+    return this;
+  }
+
+  // setAddress(address: string) {
+  //   this.data.address = {
+  //     value: address,
+  //     unrestricted_value: address,
+  //     data: {} as IAddressItem['data'],
+  //   };
+  //
+  //   return this;
+  // }
 
   setStatus(status: UStatus) {
     this.data.state = {
@@ -161,6 +238,7 @@ export class ContractorBuilder implements TFullOrganizationDataItem {
   }
 
   // TODO: вернуться и поля и методы! Критично?
+  // TODO: собрать строку адреса из `_address`
   build() {
     return this;
   }
